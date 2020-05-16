c2e0afdf414d383a04415e52b741e28b77ae662e
Jelena Cupac <j.cupac@gmail.com>
2020-05-16 21:25:13 +0200

    #18 Design - New Design

--- _site/code/java/ood/src/com/vemcodex/code/abstraction/pass/Shape.java
+++
@@ -1,7 +0,0 @@
-package com.vemcodex.code.abstraction.pass;
-
-public interface Shape {
-	
-	public double getArea();
-	
-}
--- _site/code/java/ood/src/com/vemcodex/code/encapsulation/pass/Main.java
+++
@@ -1,9 +0,0 @@
-package com.vemcodex.code.encapsulation.pass;
-
-public class Main {
-	public static void main(String[] args) {
-		BankAccount bankAccount = new BankAccount(9243242342335L, "John Smith", 4000);
-		bankAccount.deposit(5000);
-		bankAccount.withdraw(2000);
-	}
-}
